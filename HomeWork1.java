3a0ccf53d1567f73897ba55f97ba0f92bf55f463
Razvan Constantin <razvan_razvan64@yahoo.com>
2025-01-10 16:39:27 +0200

    Session-1

---
+++ HomeWork1.java
@@ -0,0 +1,53 @@
+//import java.util.Scanner;
+//
+//public class HomeWork1 {
+//    public static void main(String[] args){
+//        String Name = "Razvan";
+//        int age = 31;
+//        String Color = "Blue";
+//        System.out.println("My name is: " + Name);
+//        System.out.println("My age is: " + age);
+//        System.out.println("My favorite color is: " + Color);
+
+
+//        int x = 5;
+//        double y = 3.4d;
+//        char z = 'R';
+//        String All = "Mayo";
+//        System.out.println("x = " + x);
+//        System.out.println("y = " + y);
+//        System.out.println("z = " + z);
+//        System.out.println("String is = " + All);
+
+//        int x,y;
+//        x = 23;
+//        y = 17;
+//
+//        int rezAddition = x + y;
+//        System.out.println("Add = " + rezAddition);
+//
+//        int rezSubstraction = x - y;
+//        System.out.println("Sub = " + rezSubstraction);
+//
+//        int rezMultiplication = x * y;
+//        System.out.println("Mult = " + rezMultiplication);
+//
+//        int rezDivision = x / y;
+//        System.out.println("Div = " + rezDivision);
+//
+//        int rezModulus = x % y;
+//        System.out.println("Mod = " + rezModulus);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Insert x: ");
+//        int x = scanner.nextInt();
+//        System.out.println("Insert y: ");
+//        int y = scanner.nextInt();
+//        int sum = x + y;
+//        System.out.println("x + y = " + sum);
+//
+//
+//        scanner.close();
+//    }
+//}
+
