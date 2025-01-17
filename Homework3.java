eddf36a442c1d43550700ed778b098f298ccb59c
Razvan Constantin <razvan_razvan64@yahoo.com>
2025-01-17 10:57:33 +0200

    Homework3

---
+++ Homework3.java
@@ -0,0 +1,68 @@
+//package Session3;
+
+import java.util.Scanner;
+
+//public class Homework3 {
+//    public static void main(String[] args){
+//        // Ex 1
+
+//        int a = 5;
+//        int b = 12;
+//
+//        int rezAddition = a + b;
+//        System.out.println("Rez Add = " + rezAddition);
+//
+//        int rezSubstraction = a - b;
+//        System.out.println("Rez Sub = " + rezSubstraction);
+//
+//        int rezMultiplication = a * b;
+//        System.out.println("Rez Mult = " + rezMultiplication);
+//
+//        int rezDivision = a / b;
+//        System.out.println("Rez Div = " + rezDivision);
+//
+//        int rezModulus = a % b;
+//        System.out.println("Rez Mod = " + rezModulus);
+
+        // Ex 2
+
+//        int x = 5;
+//        System.out.println("Pre-Incremet x = " + ++x);
+//        System.out.println("New x = " + x);
+//        System.out.println("Pre-Decrement x = " + --x);
+//        System.out.println("New x = " + x);
+//        System.out.println("Post-Incremet x = " + x++);
+//        System.out.println("New x = " + x);
+//        System.out.println("Post-Decrement x = " + x--);
+//        System.out.println("New x = " + x);
+
+        // Ex 3
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Put a number: ");
+//        int number = scanner.nextInt();
+//        boolean isNotPositive = !(number > 0);
+//        if (!isNotPositive) {
+//            System.out.println("The number is positive.");
+//        } else {
+//            System.out.println("The number is negative.");
+//        }
+//        scanner.close();
+
+        // Ex 4
+
+//        System.out.println("Counting up: ");
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println("\n\nCounting down: ");
+//        for (int i = 10; i >= 1; i--) {
+//            System.out.print(i + " ");
+//        }
+
+//
+//
+//   }
+//
+//  Next exerciese are on the way!!
+//
